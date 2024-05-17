ef164ec96d517af4315ee13584a234b3dc961517
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2024-05-17 15:09:52 -0300

    Setup Serilog to project

--- src/OpenBanking.API/Program.cs
+++ src/OpenBanking.API/Program.cs
@@ -1,25 +1,63 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Setup SeriLog
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddHttpLogging(logging => {
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Open banking API",
+        Version = "v1",
+        Description = "Open banking API",
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSerilogRequestLogging();
+app.UseSwagger(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.RouteTemplate = "api/docs/{documentname}/swagger.json";
 
-app.UseHttpsRedirection();
+    options.PreSerializeFilters.Add((swagger, httpReq) =>
+    {
+        //Clear servers -element in swagger.json because it got the wrong port when hosted behind reverse proxy
+        swagger.Servers.Clear();
 
+    });
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("docs/v1/swagger.json", "Open banking API v1");
+    c.RoutePrefix = "api";
+});
+
+app.UseRouting();
+app.UseHttpLogging();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
