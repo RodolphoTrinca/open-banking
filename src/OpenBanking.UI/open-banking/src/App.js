b368115d95844ece904ba8364788614db6a6d9b2
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2024-05-20 23:46:19 -0300

    Creatint React app

---
+++ src/OpenBanking.UI/open-banking/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DrawerAppBar from './components/NavBar/DrawerAppBar';
+
+function App() {
+  return (
+    <main>
+      <DrawerAppBar/>
+    </main>
+  );
+}
+
+export default App;
